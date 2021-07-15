50454aefe8ad60d1ae1a276885c33f3c9d51aa8c
derivativedegen <derivativedegen@gmail.com>
2021-07-14 23:37:56 -0500

    successfully imported Unity instance

--- src/pages/game.js
+++ src/pages/game.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import Unity, { UnityContent } from "react-unity-webgl";
+
+const unityContent = new UnityContent(
+  "Build/Builds.json",
+  "Build/UnityLoader.js"
+);
 
 export default function Game(props) {
+  const [progression, setProgression] = useState(0);
+
+  useEffect(function () {
+    unityContent.on("progress", function (progression) {
+      setProgression(progression);
+    });
+  }, []);
+
   return (
     <div className="col-12">
-      <div className="d-flex justify-content-center">
-        <h1>Insert Game Here</h1>
+      <div className="d-flex flex-column justify-content-center">
+        <div className="col-8 offset-2 text-center">
+          <h3>Loading {progression * 100} percent...</h3>
+          <Unity unityContent={unityContent} />
+        </div>
       </div>
 
       <div class="mt-4 d-flex justify-content-center">
